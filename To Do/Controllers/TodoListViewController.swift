06945f6537eae1721d944b362728dd88a4041143
Joyce Chidiadi <joycechidiadi@joyces-air.hitronhub.home>
2018-07-11 11:16:45 -0600

    Persisting Data using CoreData

--- To Do/Controllers/TodoListViewController.swift
+++ To Do/Controllers/TodoListViewController.swift
@@ -7,38 +7,29 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
     
     var itemArray = [Item]()
     
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
-    
-    let defaults = UserDefaults.standard
+    var selectedCategory : Category? {
+        didSet {
+            loadItems()
+        }
+    }
+ 
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
      
         
-        print(dataFilePath)
-        
-//        let newItem = Item()
-//        newItem.title = "Sleep at 8 PM"
-//        itemArray.append(newItem)
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+//        let request : NSFetchRequest<Item> = Item.fetchRequest()
 //
-//        let newItem2 = Item()
-//        newItem2.title = "Buy Eggs"
-//        itemArray.append(newItem2)
-//
-//        let newItem3 = Item()
-//        newItem3.title = "Volunteer at somewhere"
-//        itemArray.append(newItem3)
 ////
-////        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-////            itemArray = items
-////        }
-//
-        loadItems()
+//        loadItems(with: request)
         
     }
     //MARK - Tableview Datasource Methods
@@ -48,6 +39,7 @@ class TodoListViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
         let item = itemArray[indexPath.row]
@@ -55,24 +47,21 @@ class TodoListViewController: UITableViewController {
         
 //        Ternary operator
         //        Value = condition ? valueIfTrue : valueIfFalse
-        
-        cell.accessoryType = item.done ? .checkmark : .none
-        
-//                                 OR USE:
-//        if item.done == true {
-//            cell.accessoryType = .checkmark
-//        } else {
-//            cell.accessoryType = .none
-//        }
-        
+
+//        cell.accessoryType = item.done ? .checkmark : .none
         return cell
     }
     
     
     //MARK - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //print(itemArray[indexPath.row]
-        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        //.remove: removesthe current item from the item array while .delete:permanently deletes from the database . The order matters a lot
+//        context.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
+       
+
+        itemArray[indexPath.row].done = itemArray[indexPath.row].done
         
         saveItems()
         
@@ -88,10 +77,11 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once the user clicks the button on the UIAlert
-            
-            let newItem = Item()
+          
+            let newItem = Item(context: self.context)
             newItem.title = textField.text!
-            
+            newItem.done = false
+            newItem.parentCategory = self.selectedCategory
             self.itemArray.append(newItem)
             self.saveItems()
 
@@ -115,26 +105,60 @@ class TodoListViewController: UITableViewController {
 //    MARK - Manipulation Methods
     
     func saveItems() {
-        let encoder = PropertyListEncoder()
+//        let encoder = PropertyListEncoder()
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            try context.save()
+         
         } catch {
-            print("Error encoding item array, \(error)")
+            print("Error saving context \(error)")
         }
         
         self.tableView.reloadData()
     }
-    func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do {
-            itemArray = try decoder.decode([Item].self, from: data)
-            } catch {
-                print("Error, decoding item array, \(error)")
-            }
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+        
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        
+        if let additionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
+        } else {
+            request.predicate = categoryPredicate
         }
+        
+        
+        do {
+            itemArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+        tableView.reloadData()
+
     }
 
 }
 
+//MARK: - Search bar methods
+extension TodoListViewController : UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        let predicate = NSPredicate(format: "title CONTAINS %@", searchBar.text!)
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        loadItems(with: request, predicate: predicate)
+  
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            
+            //this will remove the cursor and the keyboard from displaying
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            
+        }
+        
+    }
+    
+}
---
+++ To Do/Data Model/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  To Do
+//
+//  Created by Joyce Chidiadi on 2018-06-13.
+//  Copyright © 2018 Joyce Chidiadi. All rights reserved.
+//
+
+import Foundation
